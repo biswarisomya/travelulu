9be3ac5dc72186e041ec38ef160150a3a4abab1a
Somya Biswari <biswarisomya1@gmail.com>
2023-04-13 20:26:48 -0700

    removed use client

--- src/app/HomePage/Testimonials/Testimonials.tsx
+++ src/app/HomePage/Testimonials/Testimonials.tsx
@@ -1,4 +1,3 @@
-"use client";
 import SectionLayout, {
   Content,
   Description,
--- src/app/hotels/page.tsx
+++ src/app/hotels/page.tsx
@@ -1,22 +1,18 @@
-import Header from "@/app/Header/Header";
-import Footer from "@/app/Footer/Footer";
 import ContactBox from "@/app/HomePage/ContactBox/ContactBox";
 import SearchFormTop from "@/app/hotels/SearchFormTop/SearchFormTop";
-import SearchFormSide from "@/app/hotels/SearchFormSide/SearchFormSide";
-import Properties from "@/app/hotels/Properties/Properties";
 import MainPage from "@/app/hotels/MainPage/MainPage";
 import Notice from "@/app/hotels/Notice/Notice";
 import Warning from "@/app/hotels/Warning/Warning";
 
 const Page = () => {
   return (
-    <div>
+    <>
       <SearchFormTop />
       <MainPage />
       <Notice />
       <Warning />
       <ContactBox />
-    </div>
+    </>
   );
 };
 
